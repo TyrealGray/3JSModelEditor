1a2bf6d0f82a7f1483d1d5fba486a148030d48af
tyrealgray <tyrealgray@ymail.com>
2016-03-15 09:49:19 +0800

    add debug file

---
+++ nodeWebBasic.js
@@ -0,0 +1,64 @@
+var PORT = 3000;
+
+var http = require('http');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
+
+var localFileType = {
+  "css": "text/css",
+  "gif": "image/gif",
+  "html": "text/html",
+  "ico": "image/x-icon",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "js": "text/javascript",
+  "json": "application/json",
+  "pdf": "application/pdf",
+  "png": "image/png",
+  "svg": "image/svg+xml",
+  "swf": "application/x-shockwave-flash",
+  "tiff": "image/tiff",
+  "txt": "text/plain",
+  "wav": "audio/x-wav",
+  "wma": "audio/x-ms-wma",
+  "wmv": "video/x-ms-wmv",
+  "xml": "text/xml"
+};
+
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    
+    console.log("Request for " + pathname + " received.");
+    
+    var ext = path.extname(pathname);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(pathname.substr(1), function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(pathname.substr(1), "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = localFileType[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
